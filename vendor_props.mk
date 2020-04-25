08f9547d3d0a37cd88f83e283216508019a1bba8
iampro <ghosh_probir@yahoo.com>
2020-04-26 01:33:46 +0530

    RMX1921: Fix Mobile data/SIM settings reset

--- vendor_props.mk
+++ vendor_props.mk
@@ -128,4 +128,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.apm_sim_not_pwdn=1 \
     persist.vendor.radio.custom_ecc=1 \
     persist.vendor.radio.rat_on=combine \
-    persist.vendor.radio.sib16_support=1
+    persist.vendor.radio.sib16_support=1 \
+    persist.sys.fflag.override.settings_network_and_internet_v2=true \
+    persist.vendor.radio.aosp_usr_pref_sel=true
